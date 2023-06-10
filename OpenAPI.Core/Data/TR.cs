9f9647871d3d92873019f1ff0764b9e432c90edd
SANGWOO PARK <cyberprophet@hanmail.net>
2023-06-11 00:57:41 +0900

    add essential features with annotations

--- OpenAPI.Core/Data/TR.cs
+++ OpenAPI.Core/Data/TR.cs
@@ -2,22 +2,37 @@
 
 public class TR
 {
+    /// <summary>
+    /// sTrCode
+    /// </summary>
     public string? Code
     {
         get; internal set;
     }
+    /// <summary>
+    /// sRQName
+    /// </summary>
     public string? Name
     {
         get; internal set;
     }
+    /// <summary>
+    /// Input sID
+    /// </summary>
     public string[]? Input
     {
         get; internal set;
     }
+    /// <summary>
+    /// OUTPUT 싱글데이터 strItemName
+    /// </summary>
     public string[]? SingleData
     {
         get; internal set;
     }
+    /// <summary>
+    /// OUTPUT 멀티데이터 strItemName
+    /// </summary>
     public string[]? MultiData
     {
         get; internal set;
