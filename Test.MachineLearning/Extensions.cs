a612ce971f1a2799c71f5c074f93e9bd03344bfc
SANGWOO PARK <cyberprophet@hanmail.net>
2023-06-27 23:37:21 +0900

    create a project for practice

---
+++ Test.MachineLearning/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using ShareInvest.Properties;
+
+namespace ShareInvest;
+
+static class Extensions
+{
+    public static IConfigurationRoot Configuration
+    {
+        get =>
+
+            new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                      .AddJsonFile(Resources.SETTINGS)
+                                      .Build();
+    }
+}
---
+++ Test.MachineLearning/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using ShareInvest;
+using ShareInvest.Properties;
+
+using System.Media;
+
+var url = Extensions.Configuration.GetConnectionString(Resources.URI);
+
+using (var sp = new SoundPlayer(Resources.MARIO))
+{
+    sp.PlaySync();
+}
