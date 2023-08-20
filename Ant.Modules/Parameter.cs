36f7fe21c829b6cf198ba1416768ef1fa7acf48f
SANGWOO PARK <cyberprophet@hanmail.net>
2023-08-21 01:48:45 +0900

    restoring old code for CI/CD

---
+++ Ant.Modules/Parameter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ShareInvest;
+
+public static partial class Parameter
+{
+    public static string TransformOutbound(string route)
+    {
+        return Regex.Replace(route, "([a-z])([A-Z])", "$1-$2", RegexOptions.CultureInvariant, TimeSpan.FromMilliseconds(0x64))
+                    .ToLowerInvariant();
+    }
+    public static string TransformInbound(string? query)
+    {
+        if (string.IsNullOrEmpty(query) is false)
+        {
+            var str = TransformRegex().Replace(query, o => o.Groups[1].Value.ToUpper());
+
+            return string.Concat(char.ToUpper(str[0]), str[1..]);
+        }
+        return string.Empty;
+    }
+    [GeneratedRegex("-([a-z])")]
+    private static partial Regex TransformRegex();
+}
---
+++ Ant.Modules/Services/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using ShareInvest.Entities;
+
+namespace ShareInvest.Services;
+
+public interface IFileService : IDisposable
+{
+    IEnumerable<FileVersionInfo>? GetFileVersions(string app);
+
+    string Save(FileVersionInfo fileVersionInfo);
+
+    (string?, FileVersionInfo?) CheckFileVersions(string app, string name, string path);
+}
