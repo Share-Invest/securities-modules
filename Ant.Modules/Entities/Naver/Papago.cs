c62ee4a06c22db073e207d4330dc472c5d0a51de
SANGWOO PARK <cyberprophet@hanmail.net>
2023-10-30 02:06:20 +0900

    generate objects automatically

---
+++ Ant.Modules/Entities/Naver/Papago.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.Naver;
+
+public struct Papago
+{
+    [DataMember, JsonProperty("message")]
+    public TranslateSyntax TranslateSyntax
+    {
+        get; set;
+    }
+}
