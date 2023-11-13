f764a1d086c9d7a771b3ba49bd60012b1ba8a740
SANGWOO PARK <cyberprophet@hanmail.net>
2023-11-14 02:41:25 +0900

    make up the table of conclusion and balances

--- Ant.Modules/Services/IKiwoomService.cs
+++ Ant.Modules/Services/IKiwoomService.cs
@@ -1,8 +1,15 @@
-﻿using ShareInvest.OpenAPI.Entity;
+﻿using ShareInvest.Entities.Kiwoom;
+using ShareInvest.OpenAPI.Entity;
 
 namespace ShareInvest.Services;
 
 public interface IKiwoomService
 {
     Task<int> RecordAssetStatusAsync(IAccountBook assets);
+
+    Task<int> RecordAssetStatusAsync(ChejanConclusion chejan);
+
+    Task<int> RecordAssetStatusAsync(ChejanBalance chejan);
+
+    Task<int> RecordAssetStatusAsync(ChejanDerivatives chejan);
 }
