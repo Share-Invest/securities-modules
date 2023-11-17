dfdd60fc743893a70fabb35b04adb8d04983c84f
SANGWOO PARK <cyberprophet@hanmail.net>
2023-11-18 01:43:56 +0900

    make AnTalk accessible to Google

---
+++ Ant.Modules/Entities/AnTalk/AntGoogleUser.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+using ShareInvest.Entities.Google;
+
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.AnTalk;
+
+public class AntGoogleUser : GoogleUser
+{
+    [DataMember, JsonProperty("server_auth_code")]
+    public string? AuthCode
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("access_token")]
+    public string? AccessToken
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("id_token")]
+    public string? IdToken
+    {
+        get; set;
+    }
+}
