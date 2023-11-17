90adddb08fea121c6e5e29d9e2efabf7a8ee250f
SANGWOO PARK <cyberprophet@hanmail.net>
2023-11-17 17:00:54 +0900

    LogIn by authenticating with Google

---
+++ Ant.Modules/Entities/Google/GoogleUser.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.Google;
+
+public class GoogleUser
+{
+    [DataMember, JsonProperty("id")]
+    public string? Id
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("email")]
+    public string? Email
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("verified_email")]
+    public bool IsVerifiedEmail
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("name")]
+    public string? Name
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("given_name")]
+    public string? GivenName
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("family_name")]
+    public string? FamilyName
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("picture")]
+    public string? Picture
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("locale")]
+    public string? Locale
+    {
+        get; set;
+    }
+}
