7fc844e1ef00891150ca3ed5b6baa6dc8c9d22d0
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-09 01:49:36 +0900

    import friends list from KakaoTalk

---
+++ Ant.Modules/Entities/Kakao/Friend/KakaoFriend.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.Kakao;
+
+public class KakaoFriend
+{
+    [DataMember, JsonProperty("id"), StringLength(0x10), Key]
+    public string? Id
+    {
+        get; set;
+    }
+    [StringLength(0x30), Key]
+    public string? UserId
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("uuid"), StringLength(0x20)]
+    public string? Uuid
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("profileNickname"), StringLength(0x10)]
+    public string? NickName
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("profileThumbnailImage"), StringLength(0x80)]
+    public string? Image
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("favorite")]
+    public bool Favorite
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("allowedMsg")]
+    public bool AllowedMsg
+    {
+        get; set;
+    }
+    [NotMapped]
+    public DateTime RegisteredTime
+    {
+        get; set;
+    }
+    public long RegisteredTicks
+    {
+        set
+        {
+            lookup = value > Cache.Epoch ? value - Cache.Epoch : value;
+        }
+        get => lookup;
+    }
+    long lookup;
+}
---
+++ Ant.Modules/Entities/Kakao/Friend/KakaoFriendInventory.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.Kakao;
+
+public struct KakaoFriendInventory
+{
+    [DataMember, JsonProperty("elements")]
+    public IEnumerable<KakaoFriend> Friends
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("totalCount")]
+    public int Count
+    {
+        get; set;
+    }
+    [DataMember, JsonProperty("userId")]
+    public string? UserId
+    {
+        get; set;
+    }
+}
