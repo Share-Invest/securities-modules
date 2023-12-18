7387909e5255317520374535d01a33bec888781a
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-18 15:05:56 +0900

    apply overview chart in real time

---
+++ Ant.Modules/Entities/TradingView/OverviewCandleChart.cs
@@ -0,0 +1,29 @@
+﻿namespace ShareInvest.Entities.TradingView;
+
+public struct OverviewCandleChart
+{
+    public string Date
+    {
+        get; set;
+    }
+    public int Open
+    {
+        get; set;
+    }
+    public int High
+    {
+        get; set;
+    }
+    public int Low
+    {
+        get; set;
+    }
+    public int Close
+    {
+        get; set;
+    }
+    public long Volume
+    {
+        get; set;
+    }
+}
