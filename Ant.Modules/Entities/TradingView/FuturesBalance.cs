ed584696e36df72bc9302204e1fe16ef2c88e09b
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-31 22:26:58 +0900

    real-time balance implementation

---
+++ Ant.Modules/Entities/TradingView/FuturesBalance.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShareInvest.Entities.TradingView;
+
+public class FuturesBalance
+{
+    [StringLength(0x10), Required, Column(Order = 1)]
+    public string? Date
+    {
+        get; set;
+    }
+    [StringLength(8), Key]
+    public string? Code
+    {
+        get; set;
+    }
+    public double CurrentPrice
+    {
+        get; set;
+    }
+    public double PurchasePrice
+    {
+        get; set;
+    }
+    public int HoldingQuantity
+    {
+        get; set;
+    }
+    public long CumulativeRevenue
+    {
+        get; set;
+    }
+}
---
+++ Ant.Modules/Utilities/TradingView/Futures.cs
@@ -0,0 +1,56 @@
+﻿using ShareInvest.Entities.TradingView;
+
+namespace ShareInvest.Utilities.TradingView;
+
+public class Futures
+{
+    public FuturesBalance Balance
+    {
+        get;
+    }
+    public void Calculate(int quantity, double transactionPrice)
+    {
+        double liquidateReserves()
+        {
+            if (Balance.HoldingQuantity > 0 && quantity < 0)
+            {
+                var purchasePrice = transactionPrice - Balance.PurchasePrice;
+
+                if (quantity + Balance.HoldingQuantity == 0)
+                {
+                    Balance.PurchasePrice = double.NegativeZero;
+                }
+                return purchasePrice;
+            }
+            if (Balance.HoldingQuantity < 0 && quantity > 0)
+            {
+                var purchasePrice = Balance.PurchasePrice - transactionPrice;
+
+                if (quantity + Balance.HoldingQuantity == 0)
+                {
+                    Balance.PurchasePrice = double.NegativeZero;
+                }
+                return purchasePrice;
+            }
+            if (quantity != 0 && transactionPrice != 0)
+            {
+                Balance.PurchasePrice =
+
+                    (Balance.PurchasePrice * Math.Abs(Balance.HoldingQuantity) + transactionPrice) / (Math.Abs(Balance.HoldingQuantity) + Math.Abs(quantity));
+            }
+            return double.NegativeZero;
+        }
+        Balance.CumulativeRevenue += (long)(transactionMultiplier * liquidateReserves() - commission * transactionPrice * transactionMultiplier);
+        Balance.HoldingQuantity += quantity;
+    }
+    public Futures(string code, string date)
+    {
+        Balance = new FuturesBalance
+        {
+            Code = code,
+            Date = date
+        };
+    }
+    const int transactionMultiplier = 250_000;
+    const double commission = 3e-5;
+}
