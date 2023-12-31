9c043d6d8e80c4dc94296224750858e7b8c0272f
SANGWOO PARK <cyberprophet@hanmail.net>
2024-01-01 04:22:20 +0900

    request transmission of Futures Option balance data

---
+++ Ant.Modules/Entities/Assets/OPW20010.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+using ShareInvest.OpenAPI.Entity;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.Assets;
+
+public class OPW20010 : SingleOPW20010, IAccountBook
+{
+    [StringLength(0x10), DataMember, JsonProperty("계좌번호"), Key]
+    public string? AccNo
+    {
+        get; set;
+    }
+    [StringLength(10), Key]
+    public string? Date
+    {
+        get; set;
+    }
+    [Column(Order = 1)]
+    public long Lookup
+    {
+        set
+        {
+            lookup = value > Cache.Epoch ? value - Cache.Epoch : value;
+        }
+        get => lookup;
+    }
+    long lookup;
+}
---
+++ Ant.Modules/Entities/Assets/Opw20007.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+using ShareInvest.OpenAPI.Entity;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace ShareInvest.Entities.Assets;
+
+public class Opw20007 : MultiOpw20007, IAccountBook
+{
+    [StringLength(0x10), DataMember, JsonProperty("계좌번호"), Key]
+    public string? AccNo
+    {
+        get; set;
+    }
+    [StringLength(10), Key]
+    public string? Date
+    {
+        get; set;
+    }
+    [Column(Order = 1)]
+    public long Lookup
+    {
+        set
+        {
+            lookup = value > Cache.Epoch ? value - Cache.Epoch : value;
+        }
+        get => lookup;
+    }
+    [NotMapped]
+    public string? NumberOfOutputs
+    {
+        get; set;
+    }
+    long lookup;
+}
