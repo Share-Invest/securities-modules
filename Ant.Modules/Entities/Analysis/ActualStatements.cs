a62669de245d471400d6e4dca04be690988889d7
SANGWOO PARK <cyberprophet@hanmail.net>
2024-04-02 03:07:59 +0900

    rank the growth rate of each stock

---
+++ Ant.Modules/Entities/Analysis/ActualStatements.cs
@@ -0,0 +1,29 @@
+﻿namespace ShareInvest.Entities.Analysis;
+
+public struct ActualStatements
+{
+    public string Code
+    {
+        get; set;
+    }
+
+    public string Date
+    {
+        get; set;
+    }
+
+    public double Sales
+    {
+        get; set;
+    }
+
+    public double OperatingProfit
+    {
+        get; set;
+    }
+
+    public double NetProfit
+    {
+        get; set;
+    }
+}
