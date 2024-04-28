c96da706af7805ad6b5b4f05d0384d3707dd77cd
SANGWOO PARK <cyberprophet@hanmail.net>
2024-04-29 02:52:48 +0900

    discover growing and valuable stocks

---
+++ Ant.Modules/Repositories/IAnalyticsRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShareInvest.Entities;
+
+namespace ShareInvest.Repositories;
+
+public interface IAnalyticsRepository
+{
+    Analytics[] GetInOrderOfMarketCap(string? latestDate);
+}
---
+++ Ant.Modules/Services/IAnalyticsService.cs
@@ -0,0 +1,9 @@
+﻿using ShareInvest.Entities;
+using ShareInvest.Services.AnTalk;
+
+namespace ShareInvest.Services;
+
+public interface IAnalyticsService
+{
+    Task<Analytics[]> GetStocksInOrderOfRankAsync(TypeOfRankToOrder rankType);
+}
