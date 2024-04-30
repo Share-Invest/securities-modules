b5be0816cf076fbab903c96caf91269d76f46b1a
SANGWOO PARK <cyberprophet@hanmail.net>
2024-05-01 02:15:39 +0900

    implement sorting by type

--- Ant.Modules/Repositories/IAnalyticsRepository.cs
+++ Ant.Modules/Repositories/IAnalyticsRepository.cs
@@ -9,4 +9,16 @@ public interface IAnalyticsRepository
     Analytics[] GetInOrderOfMarketCap(IDictionary<string, IEnumerable<StockThemeDetail>> themes, string? latestDate);
 
     Analytics[] GetInOrderOfMarketCap(string? latestDate, int startIndex, int endIndex);
+
+    Analytics[] GetInOrderOfSales(string? latestDate, int startIndex, int endIndex);
+
+    Analytics[] GetInOrderOfOp(string? latestDate, int startIndex, int endIndex);
+
+    Analytics[] GetInOrderOfNp(string? latestDate, int startIndex, int endIndex);
+
+    Analytics[] GetInOrderOfPer(string? latestDate, int startIndex, int endIndex);
+
+    Analytics[] GetInOrderOfPbr(string? latestDate, int startIndex, int endIndex);
+
+    Analytics[] GetInOrderOfRoe(string? latestDate, int startIndex, int endIndex);
 }
