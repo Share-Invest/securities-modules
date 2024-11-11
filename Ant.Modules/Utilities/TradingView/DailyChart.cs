0389e8b377f7da398c12b3d18a76a3943a55d769
SANGWOO PARK <prophet0915@gmail.com>
2024-11-12 01:24:10 +0900

    implement the futures index chart

---
+++ Ant.Modules/Utilities/TradingView/DailyChart.cs
@@ -0,0 +1,176 @@
+﻿using ShareInvest.Entities;
+
+using Skender.Stock.Indicators;
+
+namespace ShareInvest.Utilities.TradingView;
+
+public class DailyChart(string code) : Chart(code)
+{
+    public override IEnumerable<(bool isBullish, object atrStop)> InitializedAtrStopEnumerator(AtrStopResult[]? atrStop)
+    {
+        Queue<object> data = new();
+
+        for (int index = 0; index < atrStop?.Length; index++)
+        {
+            var time = (atrStop[index].Date.Ticks - Cache.Epoch) / 10_000 / 1_000;
+
+            if (index > 0 && atrStop[index - 1].BuyStop != null != (atrStop[index].BuyStop != null))
+            {
+                yield return (atrStop[index - 1].SellStop == null, data);
+
+                data.Clear();
+            }
+            data.Enqueue(new
+            {
+                time,
+                value = atrStop[index].AtrStop
+            });
+        }
+        yield return (atrStop?[^1].SellStop == null, data);
+    }
+
+    public override object InitializedCandlestickData(IEnumerable<Quote> quotes)
+    {
+        return from e in quotes
+               select new
+               {
+                   time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+                   open = e.Open,
+                   high = e.High,
+                   low = e.Low,
+                   close = e.Close
+               };
+    }
+
+    public override (object? macd, object? signal, object? histogram) InitializedMacdData(MacdResult[]? macd)
+    {
+        var macdData = macd?.Select(e => new
+        {
+            time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+            value = e.Macd
+        });
+        var macdSignalData = macd?.Select(e => new
+        {
+            time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+            value = e.Signal
+        });
+        return (macdData, macdSignalData, macd?.Select((e, index) => new
+        {
+            time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+            value = e.Histogram,
+            color = index > 0 && macd[index - 1].Histogram < e.Histogram ? (e.Histogram > 0 ? "#F00" : "#00BFFF") : (e.Histogram > 0 ? "#800000" : "#00F")
+        }));
+    }
+
+    public override (object? linear, object? slope) InitializedSlopeData(IEnumerable<SlopeResult>? slope)
+    {
+        var linearData = slope?.Where(e => e.Line != null).Select(e => new
+        {
+            time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+            value = e.Line
+        });
+        return (linearData, slope?.Select(e => new
+        {
+            time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+            value = e.Slope
+        }));
+    }
+
+    public override IEnumerable<(bool isBullish, object superTrend)> InitializedSuperTrendEnumerator(SuperTrendResult[]? superTrend)
+    {
+        Queue<object> data = new();
+
+        for (int index = 0; index < superTrend?.Length; index++)
+        {
+            var time = (superTrend[index].Date.Ticks - Cache.Epoch) / 10_000 / 1_000;
+
+            if (index > 0 && superTrend[index - 1].UpperBand != null != (superTrend[index].UpperBand != null))
+            {
+                yield return (superTrend[index - 1].UpperBand == null, data);
+
+                data.Clear();
+            }
+            data.Enqueue(new
+            {
+                time,
+                value = superTrend[index].SuperTrend
+            });
+        }
+        yield return (superTrend?[^1].UpperBand == null, data);
+    }
+
+    public override object InitializedVolumeData(IEnumerable<Quote> quotes)
+    {
+        var quoteArr = quotes.ToArray();
+
+        return quoteArr.Select((e, index) => new
+        {
+            time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+            value = e.Volume,
+            color = index > 0 && quoteArr[index - 1].Volume < e.Volume ? "#F00" : "#00F"
+        });
+    }
+
+    public override (Series atrStop, Series superTrend, object indicator) UpdateFuturesIndicator(string code, Indicators? indicator)
+    {
+        var futuresMacd = indicator?.Macd?.ToArray();
+        var futuresSlope = indicator?.Slope?.ToArray();
+        var futuresAtrStop = indicator?.AtrStop?.ToArray();
+        var futuresSuperTrend = indicator?.SuperTrend?.ToArray();
+
+        long? macdTime = (futuresMacd?[^1].Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+              slopeTime = (futuresSlope?[^1].Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+              atrStopTime = (futuresAtrStop?[^1].Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+              superTrendTime = (futuresSuperTrend?[^1].Date.Ticks - Cache.Epoch) / 10_000 / 1_000;
+
+        return (new Series
+        {
+            ObjData = new
+            {
+                time = atrStopTime,
+                value = futuresAtrStop?[^1].AtrStop
+            },
+            IsBullish = futuresAtrStop?[^1].BuyStop != null,
+            Order = futuresAtrStop?[^1].SellStop != null
+        },
+        new Series
+        {
+            ObjData = new
+            {
+                time = superTrendTime,
+                value = futuresSuperTrend?[^1].SuperTrend
+            },
+            IsBullish = futuresSuperTrend?[^1].LowerBand != null,
+            Order = futuresSuperTrend?[^1].LowerBand != null
+        },
+        new
+        {
+            macdHistogramData = new
+            {
+                color = futuresMacd?[^2].Histogram < futuresMacd?[^1].Histogram ? (futuresMacd[^1].Histogram > 0 ? "#F00" : "#00BFFF") : (futuresMacd?[^1].Histogram > 0 ? "#800000" : "#00F"),
+                value = futuresMacd?[^1].Histogram,
+                time = macdTime
+            },
+            macdData = new
+            {
+                value = futuresMacd?[^1].Macd,
+                time = macdTime
+            },
+            macdSignalData = new
+            {
+                value = futuresMacd?[^1].Signal,
+                time = macdTime
+            },
+            slopeData = new
+            {
+                value = futuresSlope?[^1].Slope,
+                time = slopeTime
+            },
+            linearData = futuresSlope?.Where(e => e.Line != null).Select(e => new
+            {
+                time = (e.Date.Ticks - Cache.Epoch) / 10_000 / 1_000,
+                value = e.Line
+            })
+        });
+    }
+}
--- Ant.Modules/Utilities/TradingView/QuoteConvert.cs
+++ Ant.Modules/Utilities/TradingView/QuoteConvert.cs
@@ -1,4 +1,5 @@
-﻿using ShareInvest.Properties;
+﻿using ShareInvest.Entities.AnTalk;
+using ShareInvest.Properties;
 
 using Skender.Stock.Indicators;
 
@@ -18,6 +19,19 @@ public static class QuoteConvert
             Low = Math.Abs(Convert.ToDecimal(e.LowPrice)),
             Volume = Convert.ToDecimal(e.TradingVolume),
             Date = DateTime.TryParseExact(e.Time, Resources.DATETIME, null, DateTimeStyles.None, out DateTime dt) ? dt : DateTime.UnixEpoch
+        });
+    }
+
+    public static IEnumerable<Quote> GetFuturesDailyChart(AntOptionChart[] dailyChart)
+    {
+        return dailyChart.Select(e => new Quote
+        {
+            Close = Math.Abs(Convert.ToDecimal(e.Current)),
+            High = Math.Abs(Convert.ToDecimal(e.High)),
+            Open = Math.Abs(Convert.ToDecimal(e.Open)),
+            Low = Math.Abs(Convert.ToDecimal(e.Low)),
+            Volume = Convert.ToDecimal(e.Volume),
+            Date = DateTime.TryParseExact(e.Date, Resources.DATE, null, DateTimeStyles.None, out DateTime dt) ? dt : DateTime.UnixEpoch
         });
     }
 }
